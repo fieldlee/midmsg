eccd2969dc2eef0467de3fd42ea3c69b23abe288
fieldlee <ldpfieldlee@gmail.com>
2019-11-18 15:57:08 +0800

    add service middle

--- RpcSvc/main.go
+++ RpcSvc/main.go
@@ -4,10 +4,13 @@ import (
 
 	"log"
 	"net"
-
+	context "context"
 	"google.golang.org/grpc"
 
+	pb "midmsg/proto"
 )
+
+type MidService struct{}
 
 const (
 	PORT = "9002"
@@ -16,14 +19,19 @@ const (
 func main() {
 	server := grpc.NewServer()
 
-	pb.RegisterStreamServiceServer(server, &StreamService{})
+	pb.RegisterMidServiceServer(server,&MidService{})
 
-	lis, err := net.Listen("tcp", ":"+PORT)
+	listener, err := net.Listen("tcp", ":"+PORT)
 	if err != nil {
 		log.Fatalf("net.Listen err: %v", err)
 	}
-
-	server.Serve(lis)
+	server.Serve(listener)
 }
 
+func (mid *MidService)Sync(ctx context.Context,req *pb.GJ_Net_Pack)(*pb.GJ_Return_Pack,error){
+	return nil,nil
+}
 
+func (mid *MidService)Async(ctx context.Context,req *pb.GJ_Net_Pack)(*pb.GJ_Return_Pack,error){
+	return nil,nil
+}
